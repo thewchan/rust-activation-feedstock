c9f48d02ab98ed3276308a1920ac1a9733592a24
Isuru Fernando <isuruf@gmail.com>
2022-02-15 17:52:04 -0600

    Add some pkg-config specific env vars

--- recipe/activate.sh
+++ recipe/activate.sh
@@ -9,6 +9,10 @@ export RUSTUP_HOME=${CARGO_HOME}/rustup
 export CARGO_TARGET_@rust_arch_env_build@_LINKER=${CC_FOR_BUILD:-${CONDA_PREFIX}/bin/@rust_default_cc_build@}
 export CARGO_TARGET_@rust_arch_env@_LINKER=${CC:-${CONDA_PREFIX}/bin/@rust_default_cc@}
 export CARGO_BUILD_TARGET=@rust_arch@
+export CONDA_RUST_HOST=@rust_arch_env_build@
+export CONDA_RUST_TARGET=@rust_arch_env@
+export PKG_CONFIG_PATH_@rust_arch_env_build@=${CONDA_PREFIX}/lib/pkgconfig
+export PKG_CONFIG_PATH_@rust_arch_env@=${PREFIX:-${CONDA_PREFIX}}/lib/pkgconfig
 
 if [[ "@cross_target_platform@" == linux*  ]]; then
   export CARGO_BUILD_RUSTFLAGS="-C link-arg=-Wl,-rpath-link,${PREFIX:-${CONDA_PREFIX}}/lib -C link-arg=-Wl,-rpath,${PREFIX:-${CONDA_PREFIX}}/lib"
